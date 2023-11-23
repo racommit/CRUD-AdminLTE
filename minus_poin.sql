c77e069f35583c1cf26dfb202859d779de8b2f03
Rizky Agusti <64460073+racommit@users.noreply.github.com>
2023-11-23 22:05:00 +0700

    Add files via upload

--- minus_poin.sql
+++ minus_poin.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2023 at 02:35 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 23 Nov 2023 pada 16.04
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `datasensor`
+-- Struktur dari tabel `datasensor`
 --
 
 CREATE TABLE `datasensor` (
@@ -34,7 +34,7 @@ CREATE TABLE `datasensor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `datasensor`
+-- Dumping data untuk tabel `datasensor`
 --
 
 INSERT INTO `datasensor` (`id`, `data`, `waktu`) VALUES
@@ -50,7 +50,30 @@ INSERT INTO `datasensor` (`id`, `data`, `waktu`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_user`
+-- Struktur dari tabel `plus_poin`
+--
+
+CREATE TABLE `plus_poin` (
+  `id` int(11) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `poin` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `plus_poin`
+--
+
+INSERT INTO `plus_poin` (`id`, `nim`, `poin`, `keterangan`, `tanggal`) VALUES
+(7, 2202050, 20, 'Mahasiswa berprestasi', '2023-11-23'),
+(8, 2202049, 50, 'Menjuarai olimpiade', '2023-11-23'),
+(9, 2202049, 80, 'Menjadi Mahasiswa teladan', '2023-11-23');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
 --
 
 CREATE TABLE `tb_user` (
@@ -63,7 +86,7 @@ CREATE TABLE `tb_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_user`
+-- Dumping data untuk tabel `tb_user`
 --
 
 INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `no_hp`, `email`) VALUES
@@ -71,40 +94,53 @@ INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `no_hp`, `email`) V
 (2, 'tes123 Coba', 'tes123', '123', '04200802804234', 'ganteng12345'),
 (4, 'tes12311', 'tes123123', 'tes123@gmail.com', '04200802804234', '1232321'),
 (7, 'Ridwan Arif', 'ridwan123', '123', '98899', 'ddd@dddd.com'),
-(9, 'Ori Saputri', 'ori', '69', '696969', 'ori@gmail.com');
+(9, 'Ori Saputri', 'ori', '69', '696969', 'ori@gmail.com'),
+(10, 'Agusti', 'Agusti', '123', '085691964185', 'agusti@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `datasensor`
+-- Indeks untuk tabel `datasensor`
 --
 ALTER TABLE `datasensor`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_user`
+-- Indeks untuk tabel `plus_poin`
+--
+ALTER TABLE `plus_poin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `datasensor`
+-- AUTO_INCREMENT untuk tabel `datasensor`
 --
 ALTER TABLE `datasensor`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `tb_user`
+-- AUTO_INCREMENT untuk tabel `plus_poin`
+--
+ALTER TABLE `plus_poin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
